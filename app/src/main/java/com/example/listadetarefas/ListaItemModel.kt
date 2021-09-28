b21a7d89e613cd5578e7a614f1997cf8521c677c
Roberto arenhart <rbrtarenhart@gmail.com>
2021-09-28 15:17:18 -0300

    26. Criando a Classe Model do Projeto - Banco de Dados SQLite

---
+++ app/src/main/java/com/example/listadetarefas/ListaItemModel.kt
@@ -0,0 +1,30 @@
+package com.example.listadetarefas
+
+class ListaItemModel {
+    var listaId: Int = 0
+    var listaTexto: String? = null
+    var listaData: String? = null
+
+    constructor(lista_id: Int, lista_texto: String, lista_data: String) {
+        this.listaId = lista_id
+        this.listaTexto = listaTexto
+        this.listaData = listaData
+    }
+
+    companion object {
+        val LISTA_TABLE_NAME = "lista_db"
+
+        val LISTA_ID_COLUMN = "id"
+        val LISTA_TEXT_COLUMN = "listaText"
+        val LISTA_DATA_COLUMN = "listaData"
+
+        val CREATE_TABLE = (
+            "CREATE TABLE "
+                + LISTA_TABLE_NAME + "("
+                + LISTA_ID_COLUMN + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + LISTA_TEXT_COLUMN + "TEXT, "
+                + LISTA_DATA_COLUMN + "DATETIME DEFAULT CURRENT_TIMESTAMP"
+                + ")"
+        )
+    }
+}
